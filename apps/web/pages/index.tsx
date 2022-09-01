52627bd60e5347760673ee5f03de83162300e03f
kayac-chang <egg734631@gmail.com>
2022-09-02 00:52:55 +0800

    feat: 🎸 calendar init

--- apps/web/pages/index.tsx
+++ apps/web/pages/index.tsx
@@ -1,9 +1,9 @@
-import { Button } from "button";
+import { Calendar } from "calendar";
 
 export default function Web() {
   return (
     <div className="h-screen w-screen grid place-content-center">
-      <Button />
+      <Calendar />
     </div>
   );
 }
